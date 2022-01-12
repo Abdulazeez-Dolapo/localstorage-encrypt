588d7d988824b2f4f35a887f9a63b07a043f8b73
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2022-01-12 14:23:19 +0100

    improve the logic of the date functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,26 @@
 /**
  * Function to check expiry status of data in the localStorage
- * @param expiryDate: string
+ * @param expiryDate: number
+ * @return boolean
  */
 const checkExpiryDate = expiryDate => {
-	if (!expiryDate) return false
+	if (!expiryDate || typeof expiryDate !== "number") return false
 
-	const currentTime = new Date().getTime()
-	const expiryTime = new Date(expiryDate).getTime()
+	try {
+		const currentTime = new Date().getTime()
+		const expiryTime = new Date(expiryDate).getTime()
 
-	return currentTime >= expiryTime
+		return currentTime >= expiryTime
+	} catch (error) {
+		return false
+	}
 }
 
+/**
+ * Function to check expiry status of data in the localStorage
+ * @param timeInHours: number
+ * @return number
+ */
 const convertHoursToMilliseconds = timeInHours => {
 	return timeInHours * 60 * 60 * 1000
 }
@@ -18,15 +28,15 @@ const convertHoursToMilliseconds = timeInHours => {
 /**
  * Function to create expiry status of data in the localStorage
  * @param timeToExpire: number in hours
+ * @return number
  */
 const createExpiryDate = timeToExpire => {
+	if (typeof timeToExpire !== "number") return
+
 	const date = new Date().getTime()
+	const expiryDate = date + convertHoursToMilliseconds(timeToExpire)
 
-	if (typeof timeToExpire === "number") {
-		const expiryDate = date + convertHoursToMilliseconds(timeToExpire)
-
-		return new Date(expiryDate)
-	}
+	return expiryDate
 }
 
 module.exports = { checkExpiryDate, createExpiryDate }
